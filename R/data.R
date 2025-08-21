d78fcbf4b22a996c234dc3760962e6eff110f2f9
Jessalyn Sebastian <jessalys@uci.edu>
2025-08-20 17:07:23 -0700

    initial commit of 3 datasets + beginnings of docs

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' NCES High School Study 2009
+#'
+#' High school longitudinal study (add more later)
+#'
+#' @format ## `students`
+#' A data frame with 23503 rows and 37 columns:
+#' \describe{
+#'   \item{student_id}{Unique student identifier}
+#'   \item{survey1_date}{Month and year of the base survey}
+#'   ...
+#' }
+#' @source <https://nces.ed.gov/surveys/hsls09>
+"students"
